105e0083724980e7f612debcd34599b644d911e0
EvgenyKonon <konan.jack@gmail.com>
2021-12-23 15:09:49 +0300

    SchildtBookTest

---
+++ src/SchildtTest/Test1.java
@@ -0,0 +1,9 @@
+package SchildtTest;
+
+public class Test1 {
+    public static void main(String[] args) {
+        double gallons = 10.0;
+        double liters = gallons * 3.7854;
+        System.out.println(gallons+ " gallons are equal "+ liters+" L");
+    }
+}
